70dce60c7c8043f8f48274e6169c11ad351ff407
thinhjs <thinhit@yahoo.com>
2014-08-16 10:44:19 +0700

    Update detect mobile

--- mobile/js/main.js
+++ mobile/js/main.js
@@ -1,1 +1,19 @@
-
+function isMobile() { 
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
+}
+if (!isMobile()){
+	console.log('not is mobile');
+	window.location.href = "http://thinh.tk"
+}
