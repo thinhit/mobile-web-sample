18ec4afb81fff6183cf89f9a8ecd8f0c7005c71e
thinhjs <thinhit@yahoo.com>
2014-08-16 10:30:20 +0700

    update menu

--- mobile/js/plugins.js
+++ mobile/js/plugins.js
@@ -1,24 +1,13 @@
-// Avoid `console` errors in browsers that lack a console.
-(function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
+$(document).ready(function (){
+    var eMenu  = $('#menu'),
+        menuBtn = $(".navbar-toggle");
 
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
+    menuBtn.click(function (){
+        if(eMenu.hasClass('on')){
+            eMenu.removeClass('on');
+        }else{
+            eMenu.addClass('on');
         }
-    }
-}());
+    })
 
-// Place any jQuery/helper plugins in here.
+})
